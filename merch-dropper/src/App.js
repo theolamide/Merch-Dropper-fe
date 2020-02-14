f3aa34159511db3c9cf1cf07b855198629a99a9a
Oliver-Strange <preston.burton@gmail.com>
2020-02-14 09:24:46 -0800

    moves homepage location

--- merch-dropper/src/App.js
+++ merch-dropper/src/App.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 import ProductDisplay from "./components/ProductDisplay";
 import initialState from "./reducers/initialState";
 import ImageUpload from "./components/CloudinaryWidget";
-import CheckoutPage from './components/Checkout';
+import CheckoutPage from "./components/Checkout";
 import HomePage from "./components/HomePage";
 
 import DesignShirt from "./components/Shirt/DesignShirt";
@@ -19,14 +19,16 @@ function App() {
     <div className="App">
       <NavBar />
       <Switch>
-        <Route exact path="/callback" component={Callback} />
-        <Route exact path='/cart' component={ShoppingCart} />
-        <Route exact path='/checkout' component={CheckoutPage} />
-        <Route exact path='/products'
-          render={(props) => <ProductDisplay {...props} products={products} />}
-        />
-        <Route exact path='/designshirt' component={DesignShirt} />
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/callback" component={Callback} />
+        <Route exact path="/cart" component={ShoppingCart} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/products"
+          render={props => <ProductDisplay {...props} products={products} />}
+        />
+        <Route exact path="/designshirt" component={DesignShirt} />
       </Switch>
       {/* <ImageUpload /> */}
       <Footer />
