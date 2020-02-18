684d76eb835b64ddacfe04ac20a536100766dff3
Jennie Van <missjenvan@gmail.com>
2020-02-18 08:46:25 -0800

    edit to reducers test

--- merch-dropper/src/__tests__/reducers.test.js
+++ merch-dropper/src/__tests__/reducers.test.js
@@ -1,49 +1,11 @@
 import * as reducer from '../reducers';
 import CartReducer from '../reducers/CartReducers';
+import initialState from '../reducers/initialState';
 
-describe('Cart Reducers', () => {
-    // it('should add item to empty state', () => {
-    //     const stateBefore = [];
-    //     const action = {
-    //       type: 'ADD_CART_PRODUCT',
-    //       id: 0,
-    //       count: 1,
-    //     };
-    //     const stateAfter = [
-    //         {
-    //           id: 0,
-    //           count: 1,
-    //         },
-    //       ];
-    //     expect(
-    //     CartReducer(stateBefore, action)
-    //     ).toEqual(stateAfter);    
-    // });
-    it('should add item to existing state', () => {
-        let cartItems = [];
-        const stateBefore = [
-          {
-            id: 0,
-            count: 1,
-            cart: []
-          },
-        ];
-        const action = {
-          type: 'ADD_CART_PRODUCT',
-          id: 1,
-          count: 1
-        };
-        const stateAfter = [
-          ...stateBefore,
-          {
-            id: 1,
-            count: 1,
-          },
-        ];
-  
-        expect(
-          CartReducer(stateBefore, action)
-        ).toEqual(stateAfter);
-      });
-  
+
+
+describe('authenticate reducer', () => {
+  it('returns the initial state', () => {
+    expect(CartReducer(undefined, {})).toEqual(initialState);
+   });
 });
