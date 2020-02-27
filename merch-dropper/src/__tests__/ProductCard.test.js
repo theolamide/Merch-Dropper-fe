5c7b9628fe578415ffe84c44bc321e7e342e0648
theolamide <ajadidan@gmail.com>
2020-02-26 20:34:04 -0700

    ProductCard test suit completed

---
+++ merch-dropper/src/__tests__/ProductCard.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ProductCard from '../components/ProductCard';
+
+const product = {
+    url: 'www.facebook.com',
+    design: 'design',
+    color: 'red',
+    price: '$24'
+}
+
+const wrapper = shallow(<ProductCard product={product} />)
+
+it('expect to render ProductCard component', () => {
+    expect(wrapper.length).toEqual(1)
+})
+
+it('expect to verify class for corresponding html tags', () => {
+    expect(wrapper.find('button').hasClass('btn-primary cardBtn')).toBe(true);
+    expect(wrapper.find('CardBody').hasClass('product-card-padding')).toBe(true);
+    expect(wrapper.find('CardTitle').hasClass('h5 text-center')).toBe(true);
+})
+
+it('expect to verify number of specific html tags rendered', () => {
+    expect(wrapper.find('CardImg').length).toEqual(1);
+    expect(wrapper.find('CardText').length).toEqual(2);
+})
+
+it('expect to verify what type of value is the prop property', () => {
+    expect(wrapper.find(`${product.url}`)).toEqual({});
+})
+
+
+
