f6eb98781f14f26c5a20e6c9382fffde9252427f
Marcos Lira <liram1995@gmail.com>
2020-02-27 19:34:57 -0600

    Changes to NavBar and Authjs

--- merch-dropper/src/components/NavBar.js
+++ merch-dropper/src/components/NavBar.js
@@ -22,8 +22,9 @@ import {
 
 
 const NavBar = ({ hidden, history }) => {
-  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { user, isAuthenticated, loginWithRedirect, logout, getTokenSilently } = useAuth0();
   const [isOpen, setIsOpen] = useState(false);
+  const { loading } = useAuth0();
 
   const toggle = () => setIsOpen(!isOpen);
 
@@ -32,11 +33,19 @@ const NavBar = ({ hidden, history }) => {
     console.log(loading);
   }, [user]);
 
-  const { loading } = useAuth0();
-
-  // if(loading) {
-  //   return <div>Loading...</div>
+  // useEffect(() => {
+  // if( user == undefined || localStorage.getItem('profile') == "undefined")
+  // {
+  //   return;
   // }
+  // else
+  // {
+  //  const profileName = JSON.parse(localStorage.profile).name;
+  //  let profilePicture = JSON.parse(localStorage.profile).picture;
+  //  console.log(profileName);
+  //  console.log(profilePicture);
+  // }
+  // }, [localStorage.getItem('profile')]);
 
   const logoutWithRedirect = () => {
   logout({
@@ -46,67 +55,67 @@ const NavBar = ({ hidden, history }) => {
 
 
 
-  if (!user || user == "undefined") {
-    return (
-      <div className="divNav">
-        <Navbar color="white" light expand="md" className="navStyle">
-          <img
-            className="mr-5"
-            src="https://uxmasters.org/images/merch_logo_50.svg"
-            style={{ width: '2rem' }}
-          />
-          <NavbarBrand id="navTitle" href="/">
-            Merch Dropper
-          </NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <FormGroup className="searchStyle pt-3">
-                  <Input
-                    className="rounded-pill"
-                    type="search"
-                    name="search"
-                    id="exampleSearch"
-                    placeholder="Search... "
-                  />
-                </FormGroup>
-              </NavItem>
-            </Nav>
-
-            {!isAuthenticated && (
-                <button onClick={() => loginWithRedirect({})}>Log in</button>
-            )}
-
-            <Button color="primary" className="designBtn"
-              color="primary" className="designBtn"
-              onClick={() => {
-                history.push('/designshirt');
-              }}
-            >
-              Design Merch
-            </Button>
-
-            <Button color="primary" className="designBtn"
-              onClick={() => {
-                history.push('/products');
-              }}
-            >
-              Buy Merch
-            </Button>
-
-            <CartIcon />
-
-          </Collapse>
-          {hidden ? null :
-            <CartDropDown />
-          }
-        </Navbar>
-      </div>
-    );
-
-  } else {
+  // if (localStorage .getItem("profile") == "undefined") {
+  //   return (
+  //     <div className="divNav">
+  //       <Navbar color="white" light expand="md" className="navStyle">
+  //         <img
+  //           className="mr-5"
+  //           src="https://uxmasters.org/images/merch_logo_50.svg"
+  //           style={{ width: '2rem' }}
+  //         />
+  //         <NavbarBrand id="navTitle" href="/">
+  //           Merch Dropper
+  //         </NavbarBrand>
+  //         <NavbarToggler onClick={toggle} />
+  //
+  //         <Collapse isOpen={isOpen} navbar>
+  //           <Nav className="mr-auto" navbar>
+  //             <NavItem>
+  //               <FormGroup className="searchStyle pt-3">
+  //                 <Input
+  //                   className="rounded-pill"
+  //                   type="search"
+  //                   name="search"
+  //                   id="exampleSearch"
+  //                   placeholder="Search... "
+  //                 />
+  //               </FormGroup>
+  //             </NavItem>
+  //           </Nav>
+  //
+  //           {!isAuthenticated && (
+  //               <button onClick={() => loginWithRedirect({})}>Log in</button>
+  //           )}
+  //
+  //           <Button color="primary" className="designBtn"
+  //             color="primary" className="designBtn"
+  //             onClick={() => {
+  //               history.push('/designshirt');
+  //             }}
+  //           >
+  //             Design Merch
+  //           </Button>
+  //
+  //           <Button color="primary" className="designBtn"
+  //             onClick={() => {
+  //               history.push('/products');
+  //             }}
+  //           >
+  //             Buy Merch
+  //           </Button>
+  //
+  //           <CartIcon />
+  //
+  //         </Collapse>
+  //         {hidden ? null :
+  //           <CartDropDown />
+  //         }
+  //       </Navbar>
+  //     </div>
+  //   );
+  //
+  // } else {
     return (
       <div className="divNav">
         <Navbar color="white" light expand="md" className="navStyle">
@@ -138,14 +147,14 @@ const NavBar = ({ hidden, history }) => {
             <Button className="ml-5" outline color="primary" href="/">
               Buy Merch
             </Button>
-            <img src={user.picture} />
-            <h4>{user.name}</h4>
+            {/*<img src={JSON.parse(localStorage.profile).picture} />*/}
+            {/*<h4>{JSON.parse(localStorage.profile).name}</h4>*/}
             {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
           </Collapse>
         </Navbar>
       </div>
     );
-  }
+  // }
 }
 
 const mapStateToProps = (state) => ({
