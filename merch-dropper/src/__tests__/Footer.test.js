a1169ed708c6ae5add50bb2350eed9f32800d3ea
mpaolodr <mpaolodr_1504@yahoo.com>
2020-04-23 15:35:30 -0400

    edited tests for footer

--- merch-dropper/src/__tests__/Footer.test.js
+++ merch-dropper/src/__tests__/Footer.test.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import { configure, shallow } from "enzyme";
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import { configure, mount } from "enzyme";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import { store } from "../store/store.js";
+import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
 
+it("expect to render Footer component", () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/"]}>
+        <Footer />
+      </MemoryRouter>
+    </Provider>
+  );
 
-const wrapper = shallow(<Footer />)
+  const div = wrapper.find("div").get(0);
+  const ul = wrapper.find("ul a");
 
-it('expect to render Footer component', () => {
-    expect(wrapper.length).toEqual(1)
-})
-
-it('expect to verify class for corresponding html tags', () => {
-    expect(wrapper.find('Navbar').hasClass('navStyle')).toBe(true);
-    expect(wrapper.find('p').hasClass('pt-3 pl-5 ml-auto')).toBe(true);
+  expect(wrapper).toBeTruthy();
+  expect(div.props.style).toHaveProperty("display", "block");
+  expect(ul).toHaveLength(2);
+  expect(ul.containsMatchingElement(<a>Home</a>)).toBeTruthy();
+  expect(ul.containsMatchingElement(<a>Store</a>)).toBeTruthy();
 });
 
+it("has display:none style on '/stripe-setup' route", () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/stripe-setup"]}>
+        <Footer />
+      </MemoryRouter>
+    </Provider>
+  );
 
-it('expect to verify number of specific html tags rendered', () => {
-    expect(wrapper.find('NavLink').length).toEqual(2);
+  const div = wrapper.find("div").get(0);
+
+  expect(div.props.style).toHaveProperty("display", "none");
+});
+
+it("has display:none style on '/createstore' route", () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/createstore"]}>
+        <Footer />
+      </MemoryRouter>
+    </Provider>
+  );
+
+  const div = wrapper.find("div").get(0);
+  expect(div.props.style).toHaveProperty("display", "none");
 });
