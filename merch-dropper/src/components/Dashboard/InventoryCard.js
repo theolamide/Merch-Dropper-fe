d9f8602714e427a698d4977f7353785d36595c4b
Rick Ahlgren <soul@Ricks-MBP.attlocal.net>
2020-04-22 20:00:11 -0400

    Added cool delete button with animation

--- merch-dropper/src/components/Dashboard/InventoryCard.js
+++ merch-dropper/src/components/Dashboard/InventoryCard.js
@@ -2,13 +2,49 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import axiosWithAuth from "../../utils/axiosWithAuth";
 
-let style = {
-  margin: "16px 16px",
+const imgStyle = {
   width: "352px",
+};
+
+const cardContainer = {
+  width: "352px",
+  margin: "16px 32px",
   textAlign: "center",
 };
 
+const hover = {
+  width: "200px",
+  height: "48px",
+  color: "#FF3030",
+  border: "3px solid #FF3030",
+  boxSizing: "border-box",
+  borderRadius: "8px",
+  transition: "opacity ease 1s",
+};
+
+const hide = {
+  width: "200px",
+  height: "48px",
+  color: "#FF3030",
+  border: "3px solid #FF3030",
+  boxSizing: "border-box",
+  borderRadius: "8px",
+  opacity: "0",
+  transition: "opacity ease 1s",
+  // display: "none",
+};
+
+const buttonContainer = {
+  maxWidth: "100%",
+  height: "48px",
+  margin: "10px 10px",
+};
+
 const InventoryCard = (props) => {
+  const [imgIsHovered, setImgIsHovered] = useState(false);
+
+  console.log("This is imgIsHovered: ", imgIsHovered);
+
   const deleteItem = (e) => {
     e.preventDefault();
     console.log("This is props: ", props);
@@ -23,9 +59,34 @@ const InventoryCard = (props) => {
   };
 
   return (
-    <div style={style}>
-      <img src={props.fullSizeURL} style={style} />
-      <button onClick={deleteItem}>Delete Product</button>
+    <div style={cardContainer}>
+      <img
+        onMouseEnter={() => setImgIsHovered(true)}
+        onMouseLeave={() => setImgIsHovered(false)}
+        src={props.fullSizeURL}
+        style={imgStyle}
+      />
+      <div style={buttonContainer}>
+        {imgIsHovered ? (
+          <button
+            onMouseEnter={() => setImgIsHovered(true)}
+            onMouseLeave={() => setImgIsHovered(false)}
+            onClick={deleteItem}
+            style={hover}
+          >
+            Delete listing
+          </button>
+        ) : (
+          <button
+            onMouseEnter={() => setImgIsHovered(true)}
+            onMouseLeave={() => setImgIsHovered(false)}
+            onClick={deleteItem}
+            style={hide}
+          >
+            Delete listing
+          </button>
+        )}
+      </div>
     </div>
   );
 };
