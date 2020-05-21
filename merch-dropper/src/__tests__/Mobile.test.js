ac30d0f3ef474aac9923f77c8ff9931fdfc72652
JW Nicholson <john.nicholson812@gmail.com>
2020-05-21 18:13:46 -0400

    wrote test to check .MobileWrapper style rule

--- merch-dropper/src/__tests__/Mobile.test.js
+++ merch-dropper/src/__tests__/Mobile.test.js
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
+//import Adapter from "enzyme-adapter-react-16";
 
 import Navbar from '../components/NavBar';
 
-const resizeWindow = (x, y) => {
-    window.innerWidth = x;
-    window.innerHeight = y;
-    window.dispatchEvent(new Event('resize'));
-}
+// const resizeWindow = (x, y) => {
+//     window.innerWidth = x;
+//     window.innerHeight = y;
+//     window.dispatchEvent(new Event('resize'));
+// }
 
-
+describe('Navbar', () => {
+    it('should not display Hamburger @ 769px screen width', () => {
+        const mobileWrapper = shallow(
+            <Navbar>
+                <div className="MobileWrapper" />
+            </Navbar>);
+             expect(mobileWrapper.toHaveStyleRule('display', 'none'));
+    });
+   
+})
   
