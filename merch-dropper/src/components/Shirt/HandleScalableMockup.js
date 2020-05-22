04ab5aa9a2b7388da462241815f546166e5b6d74
killinq-joke <zakadmaster@gmail.com>
2020-05-22 23:46:48 +0200

    Cleaned HandleScalableMockup.js

--- merch-dropper/src/components/Shirt/HandleScalableMockup.js
+++ merch-dropper/src/components/Shirt/HandleScalableMockup.js
@@ -1,35 +1,32 @@
 import axios from "axios";
 import scalableData from "./scalableData";
 
-const HandleScalableMockup = async (garment, setGarment) => {
-  async function makeShirt() {
-    try {
-      // this code calls the scalableData component to create a garment object to be sent to Scalable Press as "data" in the axios.post call below
-      const data = await scalableData(garment);
-
+export default function HandleScalableMockup(garment, setGarment) {
+  // this code calls the scalableData component to create a garment object to be sent to Scalable Press as "data" in the axios.post call below
+  scalableData(garment)
+    .then((data) => {
       // shirtImage saves/posts the shirt preview to the back end
-      const shirtImage = await axios.post(
-        "https://merchdropper-production.herokuapp.com/api/products/mockup",
-        data,
-        {
-          headers: {
-            "Content-Type": "application/json"
+      axios
+        .post(
+          "https://merchdropper-production.herokuapp.com/api/products/mockup",
+          data,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
           }
-        }
-      );
-      const response = shirtImage;
-      console.log(response)
-      // setGarment spreads in the existing garment state object and sets the URL of the mock up image (shirtImage) to mockURL, a property
-      setGarment({ ...garment, mockUrl: response.data.URL });
-    } catch (err) {
-      console.log("ERROR:", err);
-    }
-  }
-
-  // this code executes the async function above
-  makeShirt();
+        )
+        .then((res) => {
+          // setGarment spreads in the existing garment state object and sets the URL of the mock up image (shirtImage) to mockURL, a property
+          setGarment({ ...garment, mockUrl: res.data.URL });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
   return null;
 };
-
-export default HandleScalableMockup;
