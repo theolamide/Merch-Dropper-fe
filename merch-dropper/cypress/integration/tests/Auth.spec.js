423fb5def835f52966fbb852c09871e82c9e5d88
George Davila <george.davilagaleas@gmail.com>
2020-05-26 18:20:57 -0400

    auth and sample tests

---
+++ merch-dropper/cypress/integration/tests/Auth.spec.js
@@ -0,0 +1,9 @@
+context('Cypress Demo-login', () => {
+    it('login scenario', () => {
+cy.visit('https://merchdropper.store/')
+cy.get('.links:first-child').click()
+// cy.get('#email').type('georgedavila12345@yahoo.com')
+// cy.get('#password').type('Password1')
+// cy.get()
+    })
+})
