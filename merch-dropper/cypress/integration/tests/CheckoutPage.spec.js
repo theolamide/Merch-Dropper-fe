634aa137e30f03f806ad2c13a723c22c093d62af
JW Nicholson <john.nicholson812@gmail.com>
2020-05-29 19:30:14 -0400

    added pseudocode for future tests

--- merch-dropper/cypress/integration/tests/CheckoutPage.spec.js
+++ merch-dropper/cypress/integration/tests/CheckoutPage.spec.js
@@ -1,14 +1,23 @@
 ///Tests if the checkout pay button is clickable
 describe('Checkout Page', () => {
 
- before(()=> {
+ beforeEach(()=> {
     cy.visit('http://localhost:3000/null/checkout')
  })
+
+ //add check if modal is visible
 
         it('clicks the Pay button', () => {
            
             cy.get('.StripeCheckout').click()
+
+            //add check if modal is visible after click
         })
+
+        //get inputs and and give them aliases
+        //type into form
+        //assert clicking send but is successful
+       
          
    
     })
