ade4c21e098a7d03b2a1c6d72aca4dbeff784bb9
JW Nicholson <john.nicholson812@gmail.com>
2020-06-01 12:46:26 -0400

    Working CheckoutPage.spec.js. Note- Modal visiblity tests need refactoring, as all modal classes come back unrefined

--- merch-dropper/cypress/integration/tests/CheckoutPage.spec.js
+++ merch-dropper/cypress/integration/tests/CheckoutPage.spec.js
@@ -3,20 +3,31 @@ describe('Checkout Page', () => {
 
  beforeEach(()=> {
     cy.visit('http://localhost:3000/null/checkout')
+
+    //add check if modal is visible
+    //*** cypress shows all Modal classes to be undefined ***/
+    cy.get('.ModalContainer').should('not.be.visible')
+
+    cy.get('.StripeCheckout').should('be.visible')
+    cy.get('.checkout-header').should('be.visible')
+    cy.get('.total').should('be.visible')
  })
 
- //add check if modal is visible
+ 
+        
 
         it('clicks the Pay button', () => {
            
             cy.get('.StripeCheckout').click()
 
             //add check if modal is visible after click
+             //*** cypress shows all Modal classes to be undefined  - test code needs fixed***/
+             cy.get('.ModalContainer').should('not.be.visible')
         })
 
         //get inputs and and give them aliases
         //type into form
-        //assert clicking send but is successful
+        //assert clicking send button is successful
        
          
    
