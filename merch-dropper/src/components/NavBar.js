9d0241301eb90ed5802b3e386a76e270ad1c81fa
zakaria touzri <zakadmaster@gmail.com>
2020-06-02 21:34:27 +0200

    Modified NavBar so logout & your store are hidden on storefront

--- merch-dropper/src/components/NavBar.js
+++ merch-dropper/src/components/NavBar.js
@@ -88,9 +88,18 @@ const NavBar = ({ hidden, history, location }) => {
   }
   
   const Nav = () => {
-    if (!!localStorage.getItem("profile")) {
+    if (domain_name === pathname.substr(1).split("/")[0]) {
       return (
         <nav className="ButtonWrapper">
+                 <Link to={`/${domain_name}`} className="links">
+                   {domain_name}
+                 </Link>
+                 <CartIcon />
+               </nav>
+      );
+    } else if (!!localStorage.getItem("profile")) {
+             return (
+               <nav className="ButtonWrapper">
           <Link to={`/${store_name}`} className="links">
             Your Store
           </Link>
@@ -114,15 +123,6 @@ const NavBar = ({ hidden, history, location }) => {
           </span>
           <CartIcon />
         </nav>
-      );
-    } else if (domain_name === pathname.substr(1).split("/")[0]) {
-             return (
-               <nav className="ButtonWrapper">
-                 <Link to={`/${domain_name}`} className="links">
-                   {domain_name}
-                 </Link>
-                 <CartIcon />
-               </nav>
              );
            } else {
              return (
