f849f914b78a0526a78e28164bd44a5ac1caaf8e
zakaria touzri <zakadmaster@gmail.com>
2020-06-03 00:19:03 +0200

    Addition of showAdded, shows product got added

--- merch-dropper/src/components/ProductCard.js
+++ merch-dropper/src/components/ProductCard.js
@@ -1,9 +1,16 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Card, CardBody, CardTitle, CardText, CardImg, Col } from "reactstrap";
 import "../App.css";
 
 const ProductCard = ({ product, addToCart }) => {
+  const [isAdded, setIsAdded] = useState(false);
   console.log("product", product);
+  const showAdded = () => {
+    setIsAdded(true);
+    setTimeout(() => {
+      setIsAdded(false);
+    }, 3000);
+  };
   return (
     <Fragment>
       <Col xs="6" lg="4">
@@ -21,11 +28,8 @@ const ProductCard = ({ product, addToCart }) => {
               <small className="text-muted">{product.name}</small>
             </CardText>
             <CardText>${product.price}</CardText>
-            <button
-              className="btn-primary cardBtn"
-              size="sm"
-              onClick={() => addToCart(product)}>
-              Add to Cart
+            <button className="btn-primary cardBtn" size="sm" onClick={() => addToCart(product), showAdded}>
+              {isAdded ? "Added" : "Add to Cart"}
             </button>
           </CardBody>
         </Card>
