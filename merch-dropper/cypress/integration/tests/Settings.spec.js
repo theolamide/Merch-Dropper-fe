e36b6947ed1fbd539aa19e1d7421c408b7dd5f02
Landon Turner <lturner@teamworks.com>
2020-06-11 14:07:17 -0400

    settings spec created

---
+++ merch-dropper/cypress/integration/tests/Settings.spec.js
@@ -0,0 +1,6 @@
+describe('Settings Component (Development)', () => {
+    it('reroutes to landing page with no auth token', () => {
+      cy.visit("http://localhost:3000/dashboard")
+      //add element checks here for landing page
+    })
+  })
