fe3483995710bc09912054a34c8bb5110f5b21f3
JW Nicholson <john.nicholson812@gmail.com>
2020-06-15 13:19:37 -0400

    addjusted h1 and both h2 font sizes

--- merch-dropper/src/components/Home.js
+++ merch-dropper/src/components/Home.js
@@ -20,8 +20,8 @@ const Home = ({ history }) => {
           <Media className={classes.image} object src="https://res.cloudinary.com/dze74ofbf/image/upload/v1591910999/couple_z0vlls.jpg"/>
         </Col>
         <Col className={classes.column} >               
-          <h1 className={classes.text }  >Hassle free online store.</h1>                
-          <h2 style={{ fontSize: "1.7rem"}}>You handle the designs, we'll handle the rest</h2>
+          <h1 className={classes.text } style={{fontSize: "2.8rem"}}  >Hassle free online store</h1>                
+          <h2 style={{ fontSize: "1.5rem"}}>You handle the designs, we'll handle the rest.</h2>
           <p style={{ textAlign: "center" }}>
             <Button style={{background: "#4455ee", fontSize: 18, fontWeight: "bold", marginTop: 50}} className="letsGo"
               href="https://merch-dropper.auth0.com/login?state=g6Fo2SAxazdHVDVjNXBrQW83VnFQOXhEcGdNYjkyaEprQTZMMqN0aWTZIEh4ZTVFVU9jNDM0UjIzSXQ2b3J1N2ZoZmlzWVUzWGpno2NpZNkgUGIzQ3A1cHRZZ2htTlZEanVzalBtc0hQUmtKcTZSQVA&client=Pb3Cp5ptYghmNVDjusjPmsHPRkJq6RAP&protocol=oauth2&redirect_uri=https%3A%2F%2Fmerchdropper.store%2Fredirect&audience=https%3A%2F%2Fmerchdropper-production.herokuapp.com%2F&signup=true&scope=openid%20profile%20email&response_type=code&response_mode=query&nonce=MTE4ZEN5dn5wbVItR1hfdDRiSWtMSDlBMHpETEZvS3Z2VDJ4RkZtcFd%2Bcw%3D%3D&code_challenge=Zdjv1_hFiGmK_71yYOoGJV7IVG1PZ0hW1eTU9PlW2Ik&code_challenge_method=S256&auth0Client=eyJuYW1lIjoiYXV0aDAtc3BhLWpzIiwidmVyc2lvbiI6IjEuNy4wIn0%3D"
