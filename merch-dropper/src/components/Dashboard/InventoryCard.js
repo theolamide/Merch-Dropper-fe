e8d199fd38b631302122bccdac3d2fb8b3ac70b8
zakaria touzri <zakadmaster@gmail.com>
2020-06-18 00:39:23 +0200

    Addition of default to in stock

--- merch-dropper/src/components/Dashboard/InventoryCard.js
+++ merch-dropper/src/components/Dashboard/InventoryCard.js
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 import { axiosWithEnv } from "../../utils/axiosWithEnv";
-import { withStyles } from '@material-ui/core/styles';
-import Switch from '@material-ui/core/Switch';
+import { withStyles } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
 
 // The purpose of the InventoryCard component is to render each product in a user's inventory. The InventoryList component calls the InventoryCard components using a .map method as long as the inventory array length is greater than 0.
 
 const InventoryCard = (props) => {
   const [imgIsHovered, setImgIsHovered] = useState(false);
+  const [isInStock, setIsInStock] = useState(true);
   // This const sets the state for the card based on whether or not the cursor is hovering over one of the cards.
   // The intent was to give a clean way of showing / unhiding a delete/edit button. A cleaner implementation would be preferred, as we just needed to implement delete functionality and didn't have time to implement the edit/delete flow UX initially designed for us.
 
   // deleteItem is an event handler that makes an axios.delete request to the back end when a user clicks on the Delete button. This should be an axiosWithAuth request, but we ran into issues implementing axiosWithAuth in the Inventory components where the inventory would only load with plain axios calls.
   const deleteItem = (e) => {
-    console.log("made it")
+    console.log("made it");
     e.preventDefault();
     axiosWithEnv()
-      .delete(
-        `/api/products/${props.id}`
-      )
+      .delete(`/api/products/${props.id}`)
       .then((res) => {
         window.location.reload();
       })
@@ -29,15 +28,15 @@ const InventoryCard = (props) => {
       width: 28,
       height: 16,
       padding: 0,
-      display: 'flex',
+      display: "flex",
     },
     switchBase: {
       padding: 2,
       color: theme.palette.grey[500],
-      '&$checked': {
-        transform: 'translateX(12px)',
+      "&$checked": {
+        transform: "translateX(12px)",
         color: theme.palette.common.white,
-        '& + $track': {
+        "& + $track": {
           opacity: 1,
           backgroundColor: theme.palette.primary.main,
           borderColor: theme.palette.primary.main,
@@ -47,7 +46,7 @@ const InventoryCard = (props) => {
     thumb: {
       width: 12,
       height: 12,
-      boxShadow: 'none',
+      boxShadow: "none",
     },
     track: {
       border: `1px solid ${theme.palette.grey[500]}`,
@@ -61,7 +60,12 @@ const InventoryCard = (props) => {
   // This component returns a card with an image of the shirt/model and a "Delete listing" button that only appears when the cursor hovers over the image or the space below the image where the Delete button resides.
   return (
     <div style={cardContainer}>
-      <AntSwitch />
+      <h2>
+        <AntSwitch
+          checked={isInStock}
+          onChange={() => setIsInStock(!isInStock)}
+        />
+      </h2>
       <img
         onMouseEnter={() => setImgIsHovered(true)}
         onMouseLeave={() => setImgIsHovered(false)}
